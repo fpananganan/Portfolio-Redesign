d8254e078fa0c495a6e6874b9b4966b177c732f5
Rico Pananganan <fpananganan@gmail.com>
2020-03-29 15:16:05 -0700

    Scroll Back to Top Button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
+// Contact Modal
 $(function() {
     $("#contactModal").click(function(){
         $("#modalContactForm").contactModal("show")
     }); 
+}); 
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+// Back to Top Button
+const btnScrollToTop = document.querySelector("#btnScrollToTop"); 
+
+btnScrollToTop.addEventListener("click", function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow"); 
 }); 
